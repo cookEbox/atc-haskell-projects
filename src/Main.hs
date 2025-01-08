2e471af5fea4872cd412226e44dbb4454c2f3b71
Nick Cooke <nj.cooke@outlook.com>
2025-01-08 23:40:01 +0000

    refactored Player type to simplify and added turns

--- src/Main.hs
+++ src/Main.hs
@@ -2,47 +2,43 @@ module Main where
 
 import System.IO (hFlush, stdout)
 import Control.Monad (when)
-import Data.IORef (newIORef, IORef, readIORef)
+import Data.IORef (newIORef, IORef, readIORef, writeIORef)
 import System.Process (callCommand)
+import Data.List.Split (chunksOf)
 
-data Player1 = Player1 
-  { name1  :: String
-  , score1 :: Int
-  , token1 :: Token
+data ID = A | B deriving Eq
+
+instance Show ID where 
+  show A = "1"
+  show B = "2"
+
+data Player = Player 
+  { _id   :: ID
+  , name  :: String
+  , score :: Int
+  , token :: Token
   }
 
-data Player2 = Player2 
-  { name2  :: String
-  , score2 :: Int
-  , token2 :: Token
-  }
-
-instance Show Player1 where 
-  show p  = "Player 1\n    Name: " 
-         <> name1 p 
+instance Show Player where 
+  show p  = "Player "
+         <> show (_id p)
+         <> "\n    Name: " 
+         <> name p 
          <> "\n    Score: " 
-         <> show (score1 p) 
+         <> show (score p) 
          <> "\n    Token: " 
-         <> show (token1 p)
+         <> show (token p)
 
-instance Show Player2 where 
-  show p  = "Player 2\n    Name: " 
-         <> name2 p 
-         <> "\n    Score: " 
-         <> show (score2 p) 
-         <> "\n    Token: " 
-         <> show (token2 p)
+data Game = Game Player Player 
 
-data Score = Score Player1 Player2 
+instance Show Game where 
+  show (Game p1 p2) = show p1 <> "\n" <> show p2
 
-instance Show Score where 
-  show (Score p1 p2) = show p1 <> "\n" <> show p2
-
-data Coord = Turn Token | B deriving Eq
+data Coord = Turn Token | Blank deriving Eq
 
 instance Show Coord where 
   show (Turn token) = show token
-  show B            = " "
+  show Blank        = " "
 
 data Token = X | O deriving Eq
 
@@ -53,13 +49,13 @@ instance Show Token where
 type Board = [[Coord]]
 
 data GameState = GameState 
-  { score :: Score 
+  { game :: Game 
   , board :: Board
   , go    :: Token
   } deriving Show
 
 blankBoard :: Board
-blankBoard = [[B,B,B],[B,B,B],[B,B,B]]
+blankBoard = [[Blank,Blank,Blank],[Blank,Blank,Blank],[Blank,Blank,Blank]]
 
 printGame :: Board -> IO ()
 printGame = putStrLn . (<>) "\n" . unlines . addLines . fmap printRow . numbered 
@@ -67,12 +63,9 @@ printGame = putStrLn . (<>) "\n" . unlines . addLines . fmap printRow . numbered
     printRow [x,y,z] = " " <> x <> " | " <> y <> " | " <> z
     addLines [f,s,t] = [f, hor, s, hor, t ]
     numbered         = zipWith2 ifB [["1","2","3"],["4","5","6"],["7","8","9"]]
-    ifB n b          = if b == B then n else show b
+    ifB n b          = if b == Blank then n else show b
     hor              = "--- --- ---"
     zipWith2         = zipWith . zipWith
-
-printPlayers :: Score -> IO () 
-printPlayers = print
 
 main :: IO ()
 main = do
@@ -99,18 +92,18 @@ playerSetup = do
     _                -> playerSetup
     where newBoard (player1, player2) True  
             = newIORef 
-            $ updateGameState (player1, player2) (0,0) (X,O) X
+            $ updateGameState blankBoard (player1, player2) (0,0) (X,O) X
           newBoard (player1, player2) False 
             = newIORef 
-            $ updateGameState (player1, player2) (0,0) (O,X) X
+            $ updateGameState blankBoard (player1, player2) (0,0) (O,X) X
 
-updateGameState :: (String, String) -> (Int, Int) -> (Token,Token) -> Token -> GameState
-updateGameState (_name1, _name2) (_score1, _score2) (_token1, _token2) _go = 
+updateGameState :: Board -> (String, String) -> (Int, Int) -> (Token,Token) -> Token -> GameState
+updateGameState _board (_name1, _name2) (_score1, _score2) (_token1, _token2) _go = 
   GameState 
-    { score = Score 
-    ( Player1 { name1 = _name1, score1 = _score1, token1 = _token1 } ) 
-    ( Player2 { name2 = _name2, score2 = _score2, token2 = _token2 } )
-    , board = blankBoard 
+    { game = Game 
+    ( Player { _id = A, name = _name1, score = _score1, token = _token1 } ) 
+    ( Player { _id = B, name = _name2, score = _score2, token = _token2 } )
+    , board = _board 
     , go = _go
     }
 
@@ -118,24 +111,40 @@ loop :: IORef GameState -> IO ()
 loop state = do
   current <- readIORef state
   callCommand "clear" 
-  printPlayers $ score current
+  print $ game current
   printGame $ board current
-  putStr $ playerToGoName current <> " Enter command: "
+  putStr $ playerToGoName (go current) (game current) <> " Enter command: "
   hFlush stdout
   input <- getLine
-  isLooping <- handleInput input
+  isLooping <- handleInput state input
   when isLooping $ loop state
   where 
-    playerToGoName gamestate = playerToGo (score gamestate) (go gamestate)
-    playerToGo (Score p1 p2) _go = fst 
-                                 . head 
-                                 . filter ((_go ==) . snd) 
-                                 $ [(name1 p1, token1 p1), (name2 p2, token2 p2)]
+    playerToGoName _go = name 
+                       . head 
+                       . filter (\x -> token x == _go) 
+                       . playersToGo
+    playersToGo (Game p1 p2) = [p1,p2]
 
-handleInput :: String -> IO Bool
-handleInput "exit" = do
+handleInput :: IORef GameState -> String -> IO Bool
+handleInput _ "exit" = do
   putStrLn "Goodbye!"
   pure False
-handleInput input = do
-  putStrLn $ "You entered: " ++ input
-  pure True
+handleInput state input = 
+  if input `elem` ["1", "2", "3", "4", "5", "6", "7", "8", "9"] 
+  then do 
+    current <- readIORef state
+    let coord         = read input :: Int
+        _go           = go current
+        _board        = board current
+        concatBoard   = concat _board
+        replace n b g = take (n-1) b <> [g] <> drop n b
+        switch O      = X
+        switch X      = O
+    if concatBoard !! (coord -1) == Blank 
+    then do writeIORef state $ current { board = chunksOf 3 (replace coord concatBoard (Turn _go)), go = switch _go }
+            return True
+    else return True
+
+  else do
+    putStrLn $ "You entered: " ++ input
+    pure True
