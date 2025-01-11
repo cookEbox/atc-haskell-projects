087ebc5f3aa0105abe0053f4e76abce805e69db4
Nick Cooke <nj.cooke@outlook.com>
2025-01-11 17:44:33 +0000

    game quits after there is a winner

--- src/Main.hs
+++ src/Main.hs
@@ -1,55 +1,56 @@
 module Main where
 
-import System.IO (hFlush, stdout)
-import Control.Monad (when)
-import Data.IORef (newIORef, IORef, readIORef, writeIORef)
-import System.Process (callCommand)
-import Data.List.Split (chunksOf)
+import           Control.Monad   (when)
+import           Data.IORef      (IORef, newIORef, readIORef, writeIORef)
+import           Data.List.Split (chunksOf)
+import           System.IO       (hFlush, stdout)
+import           System.Process  (callCommand)
+import Data.List (transpose)
 
 data ID = A | B deriving Eq
 
-instance Show ID where 
+instance Show ID where
   show A = "1"
   show B = "2"
 
-data Player = Player 
+data Player = Player
   { _id   :: ID
   , name  :: String
   , score :: Int
   , token :: Token
   }
 
-instance Show Player where 
+instance Show Player where
   show p  = "Player "
          <> show (_id p)
-         <> "\n    Name: " 
-         <> name p 
-         <> "\n    Score: " 
-         <> show (score p) 
-         <> "\n    Token: " 
+         <> "\n    Name: "
+         <> name p
+         <> "\n    Score: "
+         <> show (score p)
+         <> "\n    Token: "
          <> show (token p)
 
-data Game = Game Player Player 
+data Game = Game Player Player
 
-instance Show Game where 
+instance Show Game where
   show (Game p1 p2) = show p1 <> "\n" <> show p2
 
 data Coord = Turn Token | Blank deriving Eq
 
-instance Show Coord where 
+instance Show Coord where
   show (Turn token) = show token
   show Blank        = " "
 
 data Token = X | O deriving Eq
 
-instance Show Token where 
+instance Show Token where
   show X = "X"
   show O = "O"
 
 type Board = [[Coord]]
 
-data GameState = GameState 
-  { game :: Game 
+data GameState = GameState
+  { game  :: Game
   , board :: Board
   , go    :: Token
   } deriving Show
@@ -58,8 +59,8 @@ blankBoard :: Board
 blankBoard = [[Blank,Blank,Blank],[Blank,Blank,Blank],[Blank,Blank,Blank]]
 
 printGame :: Board -> IO ()
-printGame = putStrLn . (<>) "\n" . unlines . addLines . fmap printRow . numbered 
-  where 
+printGame = putStrLn . (<>) "\n" . unlines . addLines . fmap printRow . numbered
+  where
     printRow [x,y,z] = " " <> x <> " | " <> y <> " | " <> z
     addLines [f,s,t] = [f, hor, s, hor, t ]
     numbered         = zipWith2 ifB [["1","2","3"],["4","5","6"],["7","8","9"]]
@@ -83,55 +84,93 @@ playerSetup = do
   player2 <- getLine
   putStr "Choose a player to go first (X): "
   hFlush stdout
-  firstPlayer <- getLine 
-  case firstPlayer of 
+  firstPlayer <- getLine
+  case firstPlayer of
     "1"              -> newBoard (player1, player2) True
     "2"              -> newBoard (player1, player2) False
     p | p == player1 -> newBoard (player1, player2) True
     p | p == player2 -> newBoard (player1, player2) False
     _                -> playerSetup
-    where newBoard (player1, player2) True  
-            = newIORef 
+    where newBoard (player1, player2) True
+            = newIORef
             $ updateGameState blankBoard (player1, player2) (0,0) (X,O) X
-          newBoard (player1, player2) False 
-            = newIORef 
+          newBoard (player1, player2) False
+            = newIORef
             $ updateGameState blankBoard (player1, player2) (0,0) (O,X) X
 
 updateGameState :: Board -> (String, String) -> (Int, Int) -> (Token,Token) -> Token -> GameState
-updateGameState _board (_name1, _name2) (_score1, _score2) (_token1, _token2) _go = 
-  GameState 
-    { game = Game 
-    ( Player { _id = A, name = _name1, score = _score1, token = _token1 } ) 
+updateGameState _board (_name1, _name2) (_score1, _score2) (_token1, _token2) _go =
+  GameState
+    { game = Game
+    ( Player { _id = A, name = _name1, score = _score1, token = _token1 } )
     ( Player { _id = B, name = _name2, score = _score2, token = _token2 } )
-    , board = _board 
+    , board = _board
     , go = _go
     }
 
 loop :: IORef GameState -> IO ()
 loop state = do
   current <- readIORef state
-  callCommand "clear" 
+  callCommand "clear"
   print $ game current
   printGame $ board current
   putStr $ playerToGoName (go current) (game current) <> " Enter command: "
   hFlush stdout
   input <- getLine
-  isLooping <- handleInput state input
-  when isLooping $ loop state
-  where 
-    playerToGoName _go = name 
-                       . head 
-                       . filter (\x -> token x == _go) 
+  stillPlaying <- handleInput state input
+  stillGoing <- checkGame state 
+  when (stillPlaying && not stillGoing) $ loop state
+  where
+    playerToGoName _go = name
+                       . head
+                       . filter (\x -> token x == _go)
                        . playersToGo
     playersToGo (Game p1 p2) = [p1,p2]
+
+checkGame :: IORef GameState -> IO Bool 
+checkGame state = do 
+  current <- readIORef state 
+  let _board      = board current
+      checkRows   = any ( \x -> all (== Turn O) x || all (== Turn X) x )
+      topLeft     = (!!0) . (!!0)
+      middle      = (!!1) . (!!1)
+      bottomRight = (!!2) . (!!2)
+      topRight    = (!!0) . (!!2)
+      bottomLeft  = (!!2) . (!!0)
+      diagonals   =
+        [ 
+        [ topLeft _board, middle _board, bottomRight _board ]
+        ,
+        [ topRight _board, middle _board, bottomLeft _board ] 
+        ]
+      allCombinations = _board <> transpose _board <> diagonals 
+  return $ checkRows allCombinations
+
+checkGame' :: GameState -> Bool 
+checkGame' current = 
+  let _board      = board current
+      checkRows   = any (== True) . fmap ( \x -> all (== Turn O) x || all (== Turn X) x )
+      topLeft     = (!!0) . (!!0)
+      middle      = (!!1) . (!!1)
+      bottomRight = (!!2) . (!!2)
+      topRight    = (!!0) . (!!2)
+      bottomLeft  = (!!2) . (!!0)
+      diagonals   =
+        [ 
+        [ topLeft _board, middle _board, bottomLeft _board ]
+        ,
+        [ topRight _board, middle _board, bottomLeft _board ] 
+        ]
+      allCombinations = _board <> transpose _board <> diagonals 
+  in checkRows allCombinations
 
 handleInput :: IORef GameState -> String -> IO Bool
 handleInput _ "exit" = do
   putStrLn "Goodbye!"
   pure False
-handleInput state input = 
-  if input `elem` ["1", "2", "3", "4", "5", "6", "7", "8", "9"] 
-  then do 
+handleInput state input =
+  if input `elem` ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  then do
     current <- readIORef state
     let coord         = read input :: Int
         _go           = go current
@@ -140,8 +179,10 @@ handleInput state input =
         replace n b g = take (n-1) b <> [g] <> drop n b
         switch O      = X
         switch X      = O
-    if concatBoard !! (coord -1) == Blank 
-    then do writeIORef state $ current { board = chunksOf 3 (replace coord concatBoard (Turn _go)), go = switch _go }
+    if concatBoard !! (coord -1) == Blank
+    then do writeIORef state $ current { board = chunksOf 3 (replace coord concatBoard (Turn _go))
+                                       , go    = switch _go
+                                       }
             return True
     else return True
 
