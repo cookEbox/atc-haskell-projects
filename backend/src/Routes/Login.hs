d0bcf3648a9f0225945a11e27ea0844768b20e1f
Nick Cooke <nj.cooke@outlook.com>
2025-06-14 01:30:56 +0100

    login refactor

--- backend/src/Routes/Login.hs
+++ backend/src/Routes/Login.hs
@@ -7,13 +7,13 @@ import           Control.Monad.IO.Class  (liftIO)
 import           Crypto.KDF.BCrypt       (validatePassword)
 import           Data.Aeson              as A
 import qualified Data.ByteString         as BS
-import Data.Text (pack)
+import           Data.Text               (Text, pack)
 import           Data.Text.Encoding      (encodeUtf8)
 import           Data.Time.Clock         (addUTCTime, getCurrentTime)
 import           Database.DB
 import           Database.Persist        hiding (Add, count)
-import           Database.Persist.Sqlite (runSqlite)
 import           Database.Persist.Sql    (fromSqlKey)
+import           Database.Persist.Sqlite (runSqlite)
 import           Prelude                 hiding (id)
 import           Shared.Functions
 import           Snap
@@ -27,9 +27,10 @@ unSetCookie' name = setCookie False name ""
 setCookie :: Bool -> BS.ByteString -> BS.ByteString -> Snap ()
 setCookie expired name val = do
   now <- liftIO getCurrentTime
-  let expires = case expired of
-                  False -> Just $ addUTCTime (60 * 60 * 24 * 7) now  -- 1 week
-                  True  -> Just now
+  let expires = 
+        case expired of
+          False -> Just $ addUTCTime (60 * 60 * 24 * 7) now  -- 1 week
+          True  -> Just now
       cookie = Cookie
         { cookieName     = name
         , cookieValue    = val
@@ -41,33 +42,42 @@ setCookie expired name val = do
         }
   modifyResponse $ addResponseCookie cookie
 
+cookiesSet :: Text -> Text -> Text -> Snap ()
+cookiesSet signed username id_ = do
+  setCookie' $ encodeUtf8 <$> ("auth"   , signed     )
+  setCookie' $ encodeUtf8 <$> ("user"   , username   )
+  setCookie' $ encodeUtf8 <$> ("id"     , id_        )
+  setCookie' $ encodeUtf8 <$> ("status" , "loggedIn" )
+
+ifMaybeUser :: Text
+            -> Text
+            -> Maybe (Entity Twits)
+            -> Snap ()
+ifMaybeUser username password (Just (Entity id twit)) =
+  if validatePassword (encodeUtf8 password) (encodeUtf8 $ twitsPassword twit)
+  then do
+    now <- liftIO getCurrentTime
+    key <- liftIO getKey
+    let token = AuthToken username now
+        signed = makeSignedToken key token
+        id_ = pack . show $ fromSqlKey id
+    cookiesSet signed username id_
+    modifyResponse $ setHeader "Content-Type" "application/json"
+    writeLBS (A.encode $ UserDetailsResp "Success")
+  else do
+    modifyResponse $ setResponseStatus 401 "Unauthorized"
+    writeLBS "{\"error\": \"Invalid credentials\"}"
+ifMaybeUser _ _ Nothing = do
+  modifyResponse $ setResponseStatus 401 "Unauthorized"
+  writeLBS "{\"error\": \"User not found\"}"
+
 login :: Snap ()
 login = do
   req <- getRequestBody
   case A.decode req of
     Just (UserDetailsReq username password) -> do
       maybeUser <- liftIO $ runSqlite "Twits.db" $ getBy (UniqueTwit username)
-      case maybeUser of
-        Just (Entity id twit) ->
-          if validatePassword (encodeUtf8 password) (encodeUtf8 $ twitsPassword twit)
-            then do
-              now <- liftIO getCurrentTime
-              key <- liftIO getKey
-              let token = AuthToken username now
-                  signed = makeSignedToken key token
-                  id_ = pack . show $ fromSqlKey id 
-              modifyResponse $ setHeader "Content-Type" "application/json"
-              setCookie' $ encodeUtf8 <$> ("auth"   , signed     )
-              setCookie' $ encodeUtf8 <$> ("user"   , username   )
-              setCookie' $ encodeUtf8 <$> ("id"     , id_        )
-              setCookie' $ encodeUtf8 <$> ("status" , "loggedIn" )
-              writeLBS (A.encode $ UserDetailsResp "Success")
-            else do
-              modifyResponse $ setResponseStatus 401 "Unauthorized"
-              writeLBS "{\"error\": \"Invalid credentials\"}"
-        Nothing -> do
-          modifyResponse $ setResponseStatus 401 "Unauthorized"
-          writeLBS "{\"error\": \"User not found\"}"
+      ifMaybeUser username password maybeUser
     Nothing -> do
       modifyResponse $ setResponseStatus 400 "Bad Request"
       writeLBS "{\"error\": \"Invalid JSON\"}"
